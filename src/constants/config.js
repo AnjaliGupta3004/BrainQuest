be89fe820c1d9c36e0fb6bb826f624b7d53e49c2
Dell <anjaligupta02099@gmail.com>
2026-03-20 17:42:20 +0530

    secret the api key

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,6 +1,5 @@
 // src/constants/config.js
 
-export const GEMINI_API_KEY = 'AIzaSyDL-RulKyPqn7bkC0bPw5WKC7B9jITIfpA';
 
 export const GEMINI_URL =
   'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent';
